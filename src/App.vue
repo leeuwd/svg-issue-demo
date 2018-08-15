c85e9211457c7bfce9d7897e30678ca1a7e7b84c
Dick de Leeuw <dick@leeuwmedia.nl>
2018-08-15 09:35:24 +0200

    Try to add inline SVG illustration.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <div style="margin-top: 3rem; background: lightgray; min-height: 10rem;">
+            <TwitterLogoSvg/>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+    import HelloWorld from './components/HelloWorld.vue';
+    // eslint-disable-next-line import/extensions
+    import TwitterLogoSvg from './assets/twitter.svg?inline';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
-};
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld,
+            TwitterLogoSvg,
+        },
+    };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
